7e1b4378ed3849f9f1e0867a7efc0df1406ffc76
Diego Arriagada <arriagada689@gmail.com>
2025-02-28 22:06:57 -0500

    home page styling

---
+++ frontend/src/components/AchievementTable.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class='m-4 md:m-0 space-y-3 col-span-2 flex flex-col items-center md:pt-6'>
+        <div class="md:w-3/5 space-y-3">
+            <div class='text-center text-2xl font-medium'>Trivia Achievements</div>
+            <div>Track your achievements and become a trivia master! To keep track of your achievements, please login or create an account.</div>
+            <table class='w-full border-separate border-spacing-0 shadow-xl rounded-md bg-white'>
+                <thead class='bg-gray-300 dark:bg-lighterPrimary border-b border-b-gray-400'>
+                    <tr>
+                        <th>Achievement</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody class=' md:text-center'>
+                    <tr>
+                        <td class='p-1'>Trivia Master</td>
+                        <td>Achieve a perfect score</td>
+                    </tr>
+                    <tr>
+                        <td class='p-1'>Trivia Wizard</td>
+                        <td>Achieve a 3-game perfect score streak</td>
+                    </tr>
+                    <tr>
+                        <td class='p-1'>Trivia Specialist</td>
+                        <td>Achieve a 5-game perfect score streak</td>
+                    </tr>
+                    <tr>
+                        <td class='p-1'>Trivia Genius</td>
+                        <td>Achieve a 10-game perfect score streak</td>
+                    </tr>
+                    <tr>
+                        <td class='p-1'>Trivia King</td>
+                        <td>Achieve a 20-game perfect score streak</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref, watchEffect } from 'vue';
-import { useRouter } from 'vue-router';
+import { ref, watchEffect, watch } from 'vue';
+import { useRouter, RouterLink } from 'vue-router';
 import { triviaCategories } from '@/data/categories';
+import AchievementTable from '@/components/AchievementTable.vue';
 
 const router = useRouter();
 
@@ -29,59 +30,106 @@ watchEffect(() => {
     sessionStorage.setItem('time_limit', timeLimit.value)
 })
 
+//handles dynamic limit
+watch([category, difficulty], async () => {
+    if(category.value !== 0){
+        try {
+            const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
+            const response = await fetch(`${apiBaseUrl}/trivia/amount?categoryId=${category.value}&difficulty=${difficulty.value}`, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            if(response.ok) {
+                const data = await response.json()
+                limit.value = data.amount
+                if(amount.value > limit.value) {
+                    amount.value = 1
+                }
+            } else {
+                const error = await response.json()
+                throw new Error(error.message)
+            }
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+})
+
 </script>
 
 <template>
     
-    <!--Prompt Section-->
-    <div className='space-y-2'>
-
-        <!--Category dropdown-->
-        <div className='text-center text-2xl font-medium'>Select <span className='text-secondary'>Category</span></div>
-        <select name="categories" id="categories" v-model="category" className='w-full text-center border dark:bg-primary dark:border-white border-black p-1 text-lg'>
-            <option :value="0">Any Category</option>
-            <option v-for="category of categories" :value="category['id']" :key="category['id']">{{ category['name'] }}</option>
-        </select>
-
-        <!--Difficulty dropdown-->
-        <div className='space-y-2'>
-            <div className='text-center text-2xl font-medium'>Select <span className='text-secondary'>Difficulty</span></div>
-            <select name="difficulty" id="difficulty" v-model="difficulty" className='w-full text-center border dark:bg-primary dark:border-white border-black p-1 text-lg'>
-              <option :value="'any'">Any Difficulty</option>
-              <option :value="'easy'">Easy</option>
-              <option :value="'medium'">Medium</option>
-              <option :value="'hard'">Hard</option>
-            </select>
+    <div class="flex flex-col-reverse md:grid grid-cols-2 md:gap-x-3 gap-y-6 mt-6 mx-3 md:mx-0">
+        <div class="flex flex-col justify-center h-fit md:h-[384px] gap-y-4">
+            <div class="font-bold text-2xl">Creating an account will keep track of your....</div>
+            <div class="space-y-2">
+                <div><i class="pi pi-chart-line mr-3 text-3xl"></i> <span class="">Game statistics (including total games, overall average, perfect game streaks and more)</span></div>
+                <div><i class="pi pi-history mr-3 text-3xl"></i> Game History</div>
+                <div><i class="pi pi-trophy mr-3 text-3xl"></i> Achievements</div>
+            </div>
+            <div class="flex justify-center gap-x-4">
+                <RouterLink to="/register" class="bg-purple-600 hover:bg-purple-500 w-fit py-1 px-3 text-white font-semibold cursor-pointer rounded-md active:scale-[98%] text-lg">Register</RouterLink>
+                <RouterLink to="/login" class="border-2 border-black hover:bg-gray-300 w-fit py-1 px-3 text-black font-semibold cursor-pointer rounded-md active:scale-[98%] text-lg">Login</RouterLink>
+            </div>
         </div>
 
-        <!--Amount dropdown-->
-        <div className='space-y-2'>
-            <div className='text-center text-2xl font-medium'>Select <span className='text-secondary'>Amount</span></div>
-            <select name="amount" id="amount" v-model="amount" className='w-full text-center border dark:bg-primary dark:border-white border-black p-1 text-lg'>
-                <option v-for="(num, index) of new Array(limit).fill(1)" :value="index + 1" :key="index">{{ index + 1 }}</option>
+        <!--Prompt Section-->
+        <div className='space-y-3 bg-white rounded-md shadow-xl p-3'>
+
+            <!--Category dropdown-->
+            <div className='text-center text-2xl font-medium'>Select <span className='text-purple-600'>Category</span></div>
+            <select name="categories" id="categories" v-model="category" className='w-full text-center border border-black p-1 text-lg rounded-md'>
+                <option :value="0">Any Category</option>
+                <option v-for="category of categories" :value="category['id']" :key="category['id']">{{ category['name'] }}</option>
             </select>
+
+            <!--Difficulty dropdown-->
+            <div className='space-y-2'>
+                <div className='text-center text-2xl font-medium'>Select <span className='text-purple-600'>Difficulty</span></div>
+                <select name="difficulty" id="difficulty" v-model="difficulty" className='w-full text-center border border-black p-1 text-lg rounded-md'>
+                <option :value="'any'">Any Difficulty</option>
+                <option :value="'easy'">Easy</option>
+                <option :value="'medium'">Medium</option>
+                <option :value="'hard'">Hard</option>
+                </select>
+            </div>
+
+            <!--Amount dropdown-->
+            <div className='space-y-2'>
+                <div className='text-center text-2xl font-medium'>Select <span className='text-purple-600'>Amount</span></div>
+                <select name="amount" id="amount" v-model="amount" className='w-full text-center border border-black p-1 text-lg rounded-md'>
+                    <option v-for="(num, index) of new Array(limit).fill(1)" :value="index + 1" :key="index">{{ index + 1 }}</option>
+                </select>
+            </div>
+
+            <!--Gamemode dropdown-->
+            <div className='space-y-2'>
+                <div className='text-center text-2xl font-medium'>Select <span className='text-purple-600'>Game Mode</span></div>
+                <select name="gamemode" id="gamemode" v-model="gamemode" className='w-full text-center border border-black p-1 text-lg rounded-md'>
+                    <option :value="'Normal'">Normal</option>
+                    <option :value="'Timed'">Timed</option>
+                </select>
+            </div>
+
+            <!--Time limit dropdown-->
+            <div v-if="gamemode === 'Timed'" class="space-y-2">
+                <div className='text-center text-2xl font-medium'>Select <span className='text-purple-600'>Time Limit</span></div>
+                <select name="time-limir" id="time-limit" v-model="timeLimit" className='w-full text-center border border-black p-1 text-lg rounded-md'>
+                    <option :value="5">5</option>
+                    <option :value="10">10</option>
+                    <option :value="15">15</option>
+                </select>
+            </div>
+
+            <div class="text-center">
+                <button @click="handleStart" class="bg-purple-600 hover:bg-purple-500 w-fit py-1 px-3 text-white font-semibold cursor-pointer rounded-md active:scale-[98%] text-lg">Start Game</button>
+            </div>
         </div>
 
-        <!--Gamemode dropdown-->
-        <div className='space-y-2'>
-            <div className='text-center text-2xl font-medium'>Select <span className='text-secondary'>Game Mode</span></div>
-            <select name="gamemode" id="gamemode" v-model="gamemode" className='w-full text-center border dark:bg-primary dark:border-white border-black p-1 text-lg'>
-                <option :value="'Normal'">Normal</option>
-                <option :value="'Timed'">Timed</option>
-            </select>
-        </div>
-
-        <!--Time limit dropdown-->
-        <div v-if="gamemode === 'Timed'" class="space-y-2">
-            <div className='text-center text-2xl font-medium'>Select <span className='text-secondary'>Time Limit</span></div>
-            <select name="time-limir" id="time-limit" v-model="timeLimit" className='w-full text-center border dark:bg-primary dark:border-white border-black p-1 text-lg'>
-                <option :value="5">5</option>
-                <option :value="10">10</option>
-                <option :value="15">15</option>
-            </select>
-        </div>
+        
     </div>
 
-    <button @click="handleStart" class="bg-green-500 text-white">Start</button>
+    <AchievementTable />
 
 </template>
